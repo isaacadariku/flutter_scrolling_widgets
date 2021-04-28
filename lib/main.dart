93d5b89acd1972f98073bd52fdba98679ed25e40
Isaac Adeni Adariku <isaacadariku05@gmail.com>
2021-04-28 04:39:18 +0100

    Added ListView Eample

--- lib/main.dart
+++ lib/main.dart
@@ -10,110 +10,95 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Scrolling Widgets',
       debugShowCheckedModeBanner: false,
-      home: SingleChildScrollViewExample(),
+      home: ListViewExample(),
     );
   }
 }
 
-class SingleChildScrollViewExample extends StatelessWidget {
-  const SingleChildScrollViewExample({Key key});
+class ListViewExample extends StatelessWidget {
+  final countryList = [
+    "Afghanistan",
+    "Albania",
+    "Algeria",
+    "American Samoa",
+    "Andorra",
+    "Angola",
+    "Anguilla",
+    "Antarctica",
+    "Antigua and Barbuda",
+    "Argentina",
+    "Armenia",
+    "Aruba",
+    "Australia",
+    "Austria",
+    "Azerbaijan",
+    "Bahamas (the)",
+    "Bahrain",
+    "Bangladesh",
+    "Barbados",
+    "Belarus",
+    "Belgium",
+    "Belize",
+    "Benin",
+    "Bermuda",
+    "Bhutan",
+    "Bolivia (Plurinational State of)",
+    "Bonaire, Sint Eustatius and Saba",
+    "Bosnia and Herzegovina",
+    "Botswana",
+    "Bouvet Island",
+    "Brazil",
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
+    return Scaffold(
         appBar: AppBar(
-          title: Text(
-            'Single Child Scroll View Example',
-            style: TextStyle(fontSize: 20),
-          ),
+          title: Text('List View Example'),
           centerTitle: true,
         ),
-        body: SingleChildScrollView(
-            padding: EdgeInsets.symmetric(horizontal: 18),
-            child: buildTextWidgetExample()),
+        body: buildListViewSeperatedExample());
+  }
+
+  Widget buildListViewCustomExample() {
+    return ListView.custom(
+      childrenDelegate: SliverChildBuilderDelegate(
+        (BuildContext context, int index) {
+          return ListTile(title: Text(countryList[index]));
+        },
+        childCount: countryList.length,
       ),
     );
   }
 
-  Widget buildTextWidgetExample() {
-    return Text(
-      " 1. Text that is too long in text format(Here Data is coming from API) >>> Zuri & I4G training in FLUTTER " +
-          "\n \n 2. Description that is too long in text format(Here Data is coming from API)  >>> Zuri & I4G training in FLUTTER " +
-          "\n \n 3. Description that is too long in text format(Here Data is coming from API) >>> Zuri & I4G training in FLUTTER " +
-          "\n \n 4. Description that is too long in text format(Here Data is coming from API) >>> Zuri & I4G training in FLUTTER " +
-          "\n \n 5. Description that is too long in text format(Here Data is coming from API) >>> Zuri & I4G training in FLUTTER " +
-          "\n \n 6. Description that is too long in text format(Here Data is coming from API) >>>  Zuri & I4G training in FLUTTER" +
-          "\n \n 7. Description that is too long in text format(Here Data is coming from API) >>>  Zuri & I4G training in FLUTTER " +
-          "\n \n 8. Description that is too long in text format(Here Data is coming from API) >>> Zuri & I4G training in FLUTTER " +
-          "\n \n 9. Description that is too long in text format(Here Data is coming from API) >>>  Zuri & I4G training in FLUTTER " +
-          "\n \n 10. Description that is too long in text format(Here Data is coming from API) >>> Zuri & I4G training in FLUTTER ",
-      style: TextStyle(
-        fontSize: 28.0,
-        color: Colors.black,
-      ),
+  Widget buildListViewSeperatedExample() {
+    return ListView.separated(
+      itemCount: countryList.length,
+      itemBuilder: (context, index) {
+        return ListTile(title: Text(countryList[index]));
+      },
+      separatorBuilder: (context, index) {
+        return Divider();
+      },
     );
   }
 
-  Widget buildRowWidgetExample() {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        Container(
-          color: Colors.yellowAccent,
-          width: 150,
-          alignment: Alignment.center,
-          child: const Text(
-            'Fixed Width Content',
-            style: TextStyle(fontSize: 24),
-          ),
-        ),
-        Container(
-          color: Colors.green,
-          width: 100,
-          height: 500,
-          alignment: Alignment.center,
-          child: const Text(
-            'Fixed Width Content',
-            style: TextStyle(fontSize: 24),
-          ),
-        ),
-        Container(
-          color: Colors.redAccent,
-          width: 150,
-          alignment: Alignment.center,
-          child: const Text(
-            'Fixed Width Content',
-            style: TextStyle(fontSize: 24),
-          ),
-        ),
-      ],
-    );
+  Widget buildListViewBuilderExample() {
+    return ListView.builder(
+        itemCount: countryList.length,
+        itemBuilder: (context, index) {
+          return ListTile(title: Text(countryList[index]));
+        });
   }
 
-  Widget buildColumnWidgetExample() {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        Container(
-          color: Colors.yellowAccent,
-          height: 400,
-          alignment: Alignment.center,
-          child: const Text(
-            'Fixed Height Content',
-            style: TextStyle(fontSize: 24),
-          ),
-        ),
-        Container(
-          color: Colors.green,
-          height: 400,
-          alignment: Alignment.center,
-          child: const Text(
-            'Fixed Height Content',
-            style: TextStyle(fontSize: 24),
-          ),
-        ),
+  Widget buildListViewExample() {
+    return ListView(
+      children: <Widget>[
+        Text('AppBar', style: TextStyle(fontSize: 90)),
+        Text('Scaffold', style: TextStyle(fontSize: 88)),
+        Text('Text', style: TextStyle(fontSize: 140)),
+        Text('Container', style: TextStyle(fontSize: 70)),
+        Text('SafeArea', style: TextStyle(fontSize: 85)),
       ],
     );
   }
